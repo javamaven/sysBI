b2be24326f74d042f5cd378b5cbf0ffef01395fa
aa5520731 <wanghanzhao@mindai.com>
2017-07-14 15:30:13 +0800

    对外运营报告~

---
+++ renren-web/src/main/webapp/js/yunying/monthly_report_yxdw.js
@@ -0,0 +1,122 @@
+$(function () {
+	initDetailTableGrid();
+	initTimeCond();
+	initExportFunction();
+	initEvent();
+	initSelectEvent();
+});
+
+
+
+function initSelectEvent(){
+	//日报，月报切换
+	$("#list_select").change(function(){
+		var select = $(this).children('option:selected').val();
+		if(select == 'vip_detail'){
+			$("#vip_detail_div").show();
+			$("#vip_count_div").hide();
+		}else if(select == 'vip_count'){
+			$("#vip_detail_div").hide();
+			$("#vip_count_div").show();
+		}
+	});
+}
+
+function initTimeCond(){
+    $("#invest_end_time").datetimepicker({
+        format: 'yyyy-mm-dd',
+        minView:'month',
+        language: 'zh-CN',
+        autoclose:true
+    }).on("click",function(){
+    });
+}
+
+function initExportFunction(){
+	$('#btn_exports').click(function(){
+		var params = getParams();
+		
+		var select = $("#list_select").children('option:selected').val();
+		if(select == 'vip_detail'){
+			executePost('../yunying/yydw/exportExcel', {'params' : JSON.stringify(params)});
+		}
+	});
+
+}
+
+function initDetailTableGrid(){
+    $("#jqGrid").jqGrid({
+//        url: '../yunying/dmreportvipuser/list',
+        datatype: "json",
+        colModel: [
+			{ label: '日期', name: 'TIME', index: '$TYPE', width: 90,align:'right' },
+			{ label: '指标名字', name: 'ZHIBIAO', index: '$NUM', width: 90 ,align:'right'}, 			
+			{ label: '金额（数量）', name: 'MONEY', index: '$SUM', width: 90 ,align:'right'}
+
+				
+        ],
+		viewrecords: true,
+        height: $(window).height()-170,
+        rowNum: 100,
+        rownumbers: true, 
+        autowidth:true,
+//        shrinkToFit: false,
+//        autoScroll: false,
+//        multiselect: false,
+        pager: "#jqGridPager",
+        jsonReader : {
+            root: "page.list",
+            page: "page.currPage",
+            total: "page.totalPage",
+            records: "page.totalCount"
+        },
+        prmNames : {
+            page:"page", 
+            rows:"limit", 
+            order: "order"
+        },
+        gridComplete:function(){
+        	//隐藏grid底部滚动条
+        }
+    });
+}
+
+
+
+var vm = new Vue({
+	el:'#rrapp',
+	data:{
+		showList: true,
+		title: null,
+		dmReportDdzRemain: {}
+	},
+	methods: {
+		reload: function (event) {
+			vm.showList = true;
+			var select = $("#list_select").children('option:selected').val();
+			if(select == 'vip_detail'){
+				$("#jqGrid").jqGrid("clearGridData");
+				$("#jqGrid").jqGrid('setGridParam',{ 
+					datatype:'json', 
+					url: '../yunying/yydw/list',
+		            postData: getParams()
+	            }).trigger("reloadGrid");
+			}else if(select == 'vip_count'){
+				$("#jqGrid_count").jqGrid("clearGridData");
+				$("#jqGrid_count").jqGrid('setGridParam',{ 
+					datatype:'json', 
+					url: '../yunying/p2p/ddylist',
+		            postData: getParams()
+	            }).trigger("reloadGrid");
+			}
+		}
+	}
+});
+
+function getParams(){
+	var params = {
+        	'invest_end_time': $("#invest_end_time").val(),
+        	'invest_month_time': $("#invest_month_time").val()
+	};
+	return params;
+}
