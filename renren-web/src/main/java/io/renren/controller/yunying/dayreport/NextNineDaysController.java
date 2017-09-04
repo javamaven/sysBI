2c7139fed6cb8e00ee5434f9d142b9ff57fb1a74
aa5520731 <wanghanzhao@mindai.com>
2017-09-04 19:06:32 +0800

    回款情况

--- renren-web/src/main/java/io/renren/controller/yunying/dayreport/NextNineDaysController.java
+++ renren-web/src/main/java/io/renren/controller/yunying/dayreport/NextNineDaysController.java
@@ -52,37 +52,7 @@ public class NextNineDaysController {
 	private UserBehaviorService userBehaviorService;
 
 	private  String reportType="未来回款与理财计划解锁情况";
-	/**
-	 * 上传文件
-	 */
-	@SuppressWarnings("unchecked")
-	@ResponseBody
-	@RequestMapping("/upload")
-	@RequiresPermissions("phonesale:upload")
-	public R upload(@RequestParam("file") MultipartFile file) {
-		try {
-			if (file.isEmpty()) {
-				throw new RRException("上传文件不能为空");
-			}
-			String[] fields = { "number", "user_name", "real_name", "register_time", "call_sale", "call_date",
-					"call_result", "is_invest", "real_invest_amount", "sale_jiangli_amount", "year_invest_amount",
-					"first_invest_time" };
-			Map<String, Object> retMap = ExcelUtil.parseExcel(multipartToFile(file), null, fields);
-			List<Map<String, Object>> list = (List<Map<String, Object>>) retMap.get("list");
-
-			// 清空表
-			String tuncate_sql = "truncate table phone_sale_excel_data ";
-			new JdbcUtil(dataSourceFactory, "oracle26").execute(tuncate_sql);
-			// 插入表
-			String sql = "insert into phone_sale_excel_data values(?,?,?,?,?,?)";
-			List<List<Object>> dataList = getInsertDataList(list);
-			new JdbcUtil(dataSourceFactory, "oracle26").batchInsert(sql, dataList);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return R.error(e.getMessage());
-		}
-		return R.ok();
-	}
+	
 
 	/**
 	 * P2P列表
@@ -98,7 +68,7 @@ public class NextNineDaysController {
 		String beforeNineDay="";
 		String beforeDaywu="";
 		int sumday=0;
-		int oneDay=0;
+		int oneDay=0; 
 		String beforeOneDay="";
 		String day="";
 		if (StringUtils.isNotEmpty(begin_time)) {
@@ -144,38 +114,7 @@ public class NextNineDaysController {
 		System.err.println("++++++++未来回款情况查询耗时：" + (l2 - l1));
 		return R.ok().put("page", pageUtil);
 	}
-	@ResponseBody
-	@RequestMapping("/ddylist")
-	@RequiresPermissions("phonesale:list")
-	public R daylist1(Integer page, Integer limit,  String end_time,String begin_time) {
-		
-		UserBehaviorUtil userBehaviorUtil = new UserBehaviorUtil(userBehaviorService);
-		userBehaviorUtil.insert(getUserId(),new Date(),"查看",reportType," ");
-		long l1 = System.currentTimeMillis();
-		int start = (page - 1) * limit;
-		int end = start + limit;
 
-		List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
-//		List<Map<String, Object>> totalList = new ArrayList<Map<String, Object>>();
-		try {
-			String path = this.getClass().getResource("/").getPath();
-			String detail_sql = FileUtil.readAsString(new File(path + File.separator + "sql/licaijihua.txt"));
-			detail_sql = detail_sql.replace("${end_time}", end_time);
-			detail_sql = detail_sql.replace("${begin_time}", begin_time);
-			List<Map<String, Object>> list = new JdbcUtil(dataSourceFactory, "oracle26").query(detail_sql);
-			resultList.addAll(list);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		int total = resultList.size();
-		PageUtils pageUtil = new PageUtils(resultList, total, limit, page);
-		long l2 = System.currentTimeMillis();
-
-		System.err.println("++++++++电销日报明细查询耗时：" + (l2 - l1));
-		return R.ok().put("page", pageUtil);
-	}
 
 	
 	
@@ -244,7 +183,7 @@ public class NextNineDaysController {
 			va.add(resultList.get(i));
 		}
 		Map<String, String> headMap = null;
-		String title = "未来九日回款情况";
+		String title = "未来回款情况";
 		headMap = getDayListExcelFields();
 
 		ExcelUtil.downloadExcelFile(title, headMap, va, response);
@@ -266,201 +205,5 @@ public class NextNineDaysController {
 	}
 
 
-	@SuppressWarnings("unchecked")
-	@ResponseBody
-	@RequestMapping("/exportExcel2")
-	public void exportMonthListExcel2(String params, HttpServletRequest request, HttpServletResponse response)
-			throws IOException {
-		
-		UserBehaviorUtil userBehaviorUtil = new UserBehaviorUtil(userBehaviorService);
-		userBehaviorUtil.insert(getUserId(),new Date(),"导出",reportType," ");
-		Map<String, Object> map = JSON.parseObject(params, Map.class);
-		String begin_time = map.get("begin_time") + "";
-		String end_time = map.get("end_time") + "";
-
-
-		long l1 = System.currentTimeMillis();
-      
-		List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
-
-		try {
-			String path = this.getClass().getResource("/").getPath();
-			String detail_sql = FileUtil.readAsString(new File(path + File.separator + "sql/licaijihua.txt"));
-			detail_sql = detail_sql.replace("${end_time}", end_time);
-			detail_sql = detail_sql.replace("${begin_time}", begin_time);
-			List<Map<String, Object>> list = new JdbcUtil(dataSourceFactory, "oracle26").query(detail_sql);
-			resultList.addAll(list);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		// 查询列表数据
-		JSONArray va = new JSONArray();
-		for (int i = 0; i < resultList.size(); i++) {
-			va.add(resultList.get(i));
-		}
-		Map<String, String> headMap = null;
-		String title = "理财计划接触锁定监控";
-		headMap = getDayListExcelFields2();
-
-		ExcelUtil.downloadExcelFile(title, headMap, va, response);
-	}
-
 	
-	private Map<String, String> getDayListExcelFields2() {
-
-		Map<String, String> headMap = new LinkedHashMap<String, String>();
-
-		headMap.put("TIME", "日期");
-		headMap.put("MONEY", "解锁金额(万元)");
-		return headMap;
-
-	}	
-	
-	
-
-
-	private static SimpleDateFormat dateSdf = new SimpleDateFormat("yyyy-MM-dd");
-
-	private List<List<Object>> getInsertDataList(List<Map<String, Object>> excelList) {
-		List<List<Object>> dataList = new ArrayList<List<Object>>();
-		List<Object> list = null;
-		for (int i = 0; i < excelList.size(); i++) {
-			list = new ArrayList<Object>();
-			Map<String, Object> map = excelList.get(i);
-			list.add(map.get("user_name") + "");
-			list.add(2);
-			try {
-				list.add(dateSdf.parse(map.get("call_date") + ""));
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			list.add(map.get("call_result") + "");// 打电话结果
-			list.add(map.get("call_sale") + "");// 电销人员
-			list.add(null);
-
-			dataList.add(list);
-		}
-		return dataList;
-	}
-
-	/**
-	 * MultipartFile 转换成File
-	 * 
-	 * @param multfile
-	 *            原文件类型
-	 * @return File
-	 * @throws IOException
-	 */
-	private File multipartToFile(MultipartFile multfile) throws IOException {
-		CommonsMultipartFile cf = (CommonsMultipartFile) multfile;
-		// 这个myfile是MultipartFile的
-		DiskFileItem fi = (DiskFileItem) cf.getFileItem();
-		File file = fi.getStoreLocation();
-		// 手动创建临时文件
-		if (file.length() < 2048) {
-			File tmpFile = new File(
-					System.getProperty("java.io.tmpdir") + System.getProperty("file.separator") + file.getName());
-			multfile.transferTo(tmpFile);
-			return tmpFile;
-		}
-		return file;
-	}
-
-	/**
-	 * 电销日报月报：列表分页列表
-	 * 
-	 * @author Administrator
-	 *
-	 */
-	class QueryListThread implements Runnable {
-		private DataSourceFactory dataSourceFactory;
-		private List<Map<String, Object>> resultList;
-		private int start;
-		private int end;
-		private String reportType;// day , month
-		private String investEndTime;
-
-		public QueryListThread(DataSourceFactory dataSourceFactory, List<Map<String, Object>> resultList, int start,
-				int end, String reportType, String investEndTime) {
-			this.dataSourceFactory = dataSourceFactory;
-			this.resultList = resultList;
-			this.start = start;
-			this.end = end;
-			this.reportType = reportType;
-			this.investEndTime = investEndTime == null ? "" : investEndTime;
-		}
-
-		@Override
-		public void run() {
-			String path = this.getClass().getResource("/").getPath();
-			String detail_sql = null;
-			try {
-				if ("day".equals(reportType)) {
-					detail_sql = FileUtil
-							.readAsString(new File(path + File.separator + "phone_sale_day_detail_sql.txt"));
-				} else if ("month".equals(reportType)) {
-					detail_sql = FileUtil
-							.readAsString(new File(path + File.separator + "phone_sale_month_detail_sql.txt"));
-				}
-				// 分页查询
-				detail_sql = detail_sql.replace("${selectSql}", "*");
-				detail_sql = detail_sql.replace("${pageStartSql}", "and RN >= " + start);
-				detail_sql = detail_sql.replace("${pageEndSql}", "and ROWNUM <= " + end);
-				detail_sql = detail_sql.replace("${investEndTime}", investEndTime);
-				List<Map<String, Object>> list = new JdbcUtil(dataSourceFactory, "oracle26").query(detail_sql);
-				resultList.addAll(list);
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	/**
-	 * 电销月报：列表总行数
-	 * 
-	 * @author Administrator
-	 *
-	 */
-	class QueryListTotalThread implements Runnable {
-		private DataSourceFactory dataSourceFactory;
-		private List<Map<String, Object>> totalList;
-		private String reportType;
-		private String investEndTime;
-
-		public QueryListTotalThread(DataSourceFactory dataSourceFactory, List<Map<String, Object>> totalList,
-				String reportType, String investEndTime) {
-			this.dataSourceFactory = dataSourceFactory;
-			this.totalList = totalList;
-			this.reportType = reportType;
-			this.investEndTime = investEndTime == null ? "" : investEndTime;
-		}
-
-		@Override
-		public void run() {
-			String path = this.getClass().getResource("/").getPath();
-			String detail_sql = null;
-			try {
-				if ("day".equals(reportType)) {
-					detail_sql = FileUtil
-							.readAsString(new File(path + File.separator + "phone_sale_day_detail_sql.txt"));
-				} else if ("month".equals(reportType)) {
-					detail_sql = FileUtil
-							.readAsString(new File(path + File.separator + "phone_sale_month_detail_sql.txt"));
-				}
-				detail_sql = detail_sql.replace("${selectSql}", "count(1) total");
-				detail_sql = detail_sql.replace("${pageStartSql}", "");
-				detail_sql = detail_sql.replace("${pageEndSql}", "");
-				detail_sql = detail_sql.replace("${investEndTime}", investEndTime);
-				totalList.addAll(new JdbcUtil(dataSourceFactory, "oracle26").query(detail_sql));
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
 }
