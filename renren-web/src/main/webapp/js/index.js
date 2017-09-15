87ddf7751cadb60cd3b46fbf1aa2d15b00593dc1
Administrator <Administrator@PC-20170330HHFX>
2017-09-15 15:35:45 +0800

    1、引入easyui框架 2、系统增加tabs菜单页

--- renren-web/src/main/webapp/js/index.js
+++ renren-web/src/main/webapp/js/index.js
@@ -1,3 +1,41 @@
+function addTab(title, href, icon){
+    var tt = $('#tabs');  
+    if (tt.tabs('exists', title)){//如果tab已经存在,则选中并刷新该tab          
+        tt.tabs('select', title);  
+        refreshTab({tabTitle:title,url:href});  
+    } else {  
+        if (href){  
+            var content = '<iframe scrolling="no" frameborder="0"  src="'+href+'" style="width:100%;height:100%;margin-top:10px;margin-left:0px;"></iframe>';  
+        } else {  
+            var content = '未实现';  
+        }  
+        tt.tabs('add',{  
+            title:title,  
+            closable:true,  
+            content:content
+//            ,  
+//            iconCls:icon||'icon-default'  
+        });  
+    }  
+}  
+/**     
+ * 刷新tab 
+ * @cfg  
+ *example: {tabTitle:'tabTitle',url:'refreshUrl'} 
+ *如果tabTitle为空，则默认刷新当前选中的tab 
+ *如果url为空，则默认以原来的url进行reload 
+ */  
+function refreshTab(cfg){  
+    var refresh_tab = cfg.tabTitle?$('#tabs').tabs('getTab',cfg.tabTitle):$('#tabs').tabs('getSelected');  
+    if(refresh_tab && refresh_tab.find('iframe').length > 0){
+    	var _refresh_ifram = refresh_tab.find('iframe')[0];  
+    	var refresh_url = cfg.url?cfg.url:_refresh_ifram.src;  
+    	_refresh_ifram.contentWindow.location.href=refresh_url;  
+    }  
+}
+
+
+
 //生成菜单
 var menuItem = Vue.extend({
 	name: 'menu-item',
@@ -104,7 +142,11 @@ function routerList(router, menuList){
 		}else if(menu.type == 1){
 			router.add('#'+menu.url, function() {
 				var url = window.location.hash;
-				
+				var menuName = $("a[href='"+url+"']").text();
+				addTab(menuName, url.replace('#', ''));
+				console.info("+++++url+++++++++++" + url)
+				console.info(menu.name)
+				console.info(menu)
 				//替换iframe的url
 			    vm.main = url.replace('#', '');
 			    
