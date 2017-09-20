0e2b595b049a3aeaeff67cc4a3056c490dd0ca9e
aa5520731 <wanghanzhao@mindai.com>
2017-09-20 13:58:29 +0800

    待收区间

--- renren-web/src/main/java/io/renren/controller/yunying/dayreport/DaiShouQuJianController.java
+++ renren-web/src/main/java/io/renren/controller/yunying/dayreport/DaiShouQuJianController.java
@@ -79,6 +79,36 @@ public class DaiShouQuJianController {
 		return R.ok().put("page", pageUtil);
 	}
 	
+	
+	@ResponseBody
+	@RequestMapping("/ddylist")
+	public R ddylist(Integer page, Integer limit) {
+		UserBehaviorUtil userBehaviorUtil = new UserBehaviorUtil(userBehaviorService);
+		userBehaviorUtil.insert(getUserId(),new Date(),"查看",reportType," ");
+		
+		long l1 = System.currentTimeMillis();
+
+		List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
+
+		try {
+			String path = this.getClass().getResource("/").getPath();
+			String detail_sql;
+			detail_sql = FileUtil.readAsString(new File(path + File.separator + "sql/年龄区间.txt"));
+			List<Map<String, Object>> list = new JdbcUtil(dataSourceFactory, "oracle26").query(detail_sql);
+			resultList.addAll(list);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int total = resultList.size();
+		PageUtils pageUtil = new PageUtils(resultList, total, limit, page);
+		long l2 = System.currentTimeMillis();
+
+		System.err.println("++++++++年龄区间查询耗时：" + (l2 - l1));
+		return R.ok().put("page", pageUtil);
+	}
+	
 	@SuppressWarnings("unchecked")
 	@ResponseBody
 	@RequestMapping("/exportExcel")
@@ -112,6 +142,39 @@ public class DaiShouQuJianController {
 	}
 
 	
+	@SuppressWarnings("unchecked")
+	@ResponseBody
+	@RequestMapping("/exportExcel2")
+	public void exportMonthListExcel2(String params, HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		UserBehaviorUtil userBehaviorUtil = new UserBehaviorUtil(userBehaviorService);
+		userBehaviorUtil.insert(getUserId(),new Date(),"导出",reportType," ");
+		
+		R r=ddylist(1, 10000);
+		PageUtils pageUtil = (PageUtils) r.get("page");	
+		
+		List<Map<String,Object>> resultList = (List<Map<String, Object>>) pageUtil.getList();
+
+		// 查询列表数据
+		JSONArray va = new JSONArray();
+		for (int i = 0; i < resultList.size(); i++) {
+			Map<String, Object> map = resultList.get(i);
+			Object num = map.get("NUM_RATE");
+			Object await = map.get("AWAIT_RATE");
+//			Object avg = map.get("AVG_APR");
+			map.put("NUM_RATE", NumberUtil.keepPrecision(Double.parseDouble(num.toString())*100, 2) +"%");
+			map.put("AWAIT_RATE", NumberUtil.keepPrecision(Double.parseDouble(await.toString())*100, 2) +"%");
+//			map.put("AVG_APR", NumberUtil.keepPrecision(Double.parseDouble(avg.toString())*100, 2) +"%");
+			va.add(map);
+		}
+		Map<String, String> headMap = null;
+		String title = "年龄区间数据";
+		headMap = getDayListExcelFields2();
+
+		ExcelUtil.downloadExcelFile(title, headMap, va, response);
+	}
+	
+	
 	private Map<String, String> getDayListExcelFields() {
 
 		Map<String, String> headMap = new LinkedHashMap<String, String>();
@@ -130,6 +193,28 @@ public class DaiShouQuJianController {
 
 	}
 
+	private Map<String, String> getDayListExcelFields2() {
+
+		Map<String, String> headMap = new LinkedHashMap<String, String>();
+
+		headMap.put("G_OLDS", "年龄区间");
+		headMap.put("NUM", "人数");
+		headMap.put("NUM_RATE", "人数占比");
+		headMap.put("AWAIT", "待收本金");
+		headMap.put("AWAIT_RATE", "待收占比");
+		headMap.put("AVG_AWAIT", "人均待收");
+		
+		headMap.put("MAN", "男性人数");
+		headMap.put("WOMAN", "女性人数");
+		headMap.put("WEIZHI", "性别未知人数");
+		
+		headMap.put("AVG_MAN_AWAIT", "男性人均待收");
+		headMap.put("AVG_WOMAN_AWAIT", "女性人均待收");
+		headMap.put("AVG_WEIZHI_AWAIT", "性别未知人均待收");
+
+		return headMap;
+
+	}
 
 
 
--- renren-web/src/main/webapp/js/yunying/daishouqujian.js
+++ renren-web/src/main/webapp/js/yunying/daishouqujian.js
@@ -2,9 +2,25 @@ $(function () {
 	initDetailTableGrid();
 	initExportFunction();
 	initEvent();
+	initSelectEvent();
+	initNianLinTableGrid();
 });
 
 
+function initSelectEvent(){
+	//日报，月报切换
+	$("#list_select").change(function(){
+		var select = $(this).children('option:selected').val();
+		if(select == 'vip_detail'){
+			$("#vip_detail_div").show();
+			$("#vip_count_div").hide();
+		}else if(select == 'nianlin'){
+			$("#vip_detail_div").hide();
+			$("#vip_count_div").show();
+		}
+	});
+}
+'../yunying/daishouqujian/exportExcel'
 
 
 function initExportFunction(){
@@ -12,10 +28,15 @@ function initExportFunction(){
 		var params = getParams();
 		
 		var select = $("#list_select").children('option:selected').val();
+		if(select == 'vip_detail'){
 			executePost('../yunying/daishouqujian/exportExcel', {'params' : JSON.stringify(params)});
+		}else if(select == 'nianlin'){
+			executePost('../yunying/daishouqujian/exportExcel2', {'params' : JSON.stringify(params)});
+		}
+		
 	});
 
-}  
+}
 function initDetailTableGrid(){
     $("#jqGrid").jqGrid({
 //        url: '../yunying/dmreportvipuser/list',
@@ -48,7 +69,7 @@ function initDetailTableGrid(){
 			{ label: '当日利息', name: 'INTEREST', index: '$NUMS', width: 90,align:'right' }
         ],
 		viewrecords: true,
-        height: $(window).height()-170,
+        height: $(window).height()-150,
         rowNum: 20,
         rownumbers: true, 
         autowidth:true,
@@ -73,6 +94,61 @@ function initDetailTableGrid(){
     });
 }
 
+function initNianLinTableGrid(){
+    $("#jqGrid_count").jqGrid({
+//        url: '../yunying/dmreportvipuser/list',
+        datatype: "json",
+        colModel: [
+        	{ label: '年龄区间', name: 'G_OLDS', index: '$TYPE', width: 90,align:'right' },
+			{ label: '人数', name: 'NUM', index: '$NUM', width: 90 ,align:'right'}, 			
+			{ label: '人数占比', name: 'NUM_RATE', index: '$SUM', width: 90 ,align:'right',
+				formatter: function(value, options, row){
+					return formatNumber(value*100,2) + '%';
+				}	
+			}, 
+			
+			
+			{ label: '待收本金', name: 'AWAIT', index: '$BORROW_USER', width: 130 ,align:'right'}, 			
+			{ label: '待收占比', name: 'AWAIT_RATE', index: '$BORROW_CAPITAL', width: 90 ,align:'right', 
+				formatter: function(value, options, row){
+					return formatNumber(value*100,2) + '%';
+				}	
+			}, 
+			{ label: '人均待收本金', name: 'AVG_AWAIT', index: '$NUMM', width: 130,align:'right' },
+			{ label: '男性人数', name: 'MAN', index: '$SUMM', width: 90 ,align:'right'},		
+			{ label: '女性人数', name: 'WOMAN', index: '$AVGG', width: 90 ,align:'right'},
+			{ label: '性别未知人数', name: 'WEIZHI', index: '$NUMS', width: 130,align:'right'},
+			{ label: '男性人均待收', name: 'AVG_MAN_AWAIT', index: '$NUMS', width: 130,align:'right'},
+			{ label: '女性人均待收', name: 'AVG_WOMAN_AWAIT', index: '$NUMS', width: 130,align:'right'},
+			{ label: '性别未知人均待收', name: 'AVG_WEIZHI_AWAIT', index: '$NUMS', width: 150,align:'right'}
+        ],
+		viewrecords: true,
+        height: $(window).height()-150,
+        rowNum: 20,
+        rownumbers: true, 
+        autowidth:true,
+//        shrinkToFit: false,
+//        autoScroll: false,
+//        multiselect: false,
+        pager: "#jqGridPager_count",
+        jsonReader : {
+            root: "page.list",
+            page: "page.currPage",
+            total: "page.totalPage",
+            records: "page.totalCount"
+        },
+        prmNames : {
+            page:"page", 
+            rows:"limit", 
+            order: "order"
+        },
+        gridComplete:function(){
+        	//隐藏grid底部滚动条
+        }
+    });
+    $("#vip_count_div").hide();
+}
+
 
 
 var vm = new Vue({
@@ -86,16 +162,47 @@ var vm = new Vue({
 		reload: function (event) {
 			vm.showList = true;
 			var select = $("#list_select").children('option:selected').val();
+			if(select == 'vip_detail'){
 				$("#jqGrid").jqGrid("clearGridData");
 				$("#jqGrid").jqGrid('setGridParam',{ 
 					datatype:'json', 
 					url: '../yunying/daishouqujian/list',
 		            postData: getParams()
 	            }).trigger("reloadGrid");
+			}else if(select == 'nianlin'){
+				$("#jqGrid_count").jqGrid("clearGridData");
+				$("#jqGrid_count").jqGrid('setGridParam',{ 
+					datatype:'json', 
+					url: '../yunying/daishouqujian/ddylist',
+		            postData: getParams()
+	            }).trigger("reloadGrid");
 			}
-		
+		}
 	}
 });
+
+
+//var vm = new Vue({
+//	el:'#rrapp',
+//	data:{
+//		showList: true,
+//		title: null,
+//		dmReportDdzRemain: {}
+//	},
+//	methods: {
+//		reload: function (event) {
+//			vm.showList = true;
+//			var select = $("#list_select").children('option:selected').val();
+//				$("#jqGrid").jqGrid("clearGridData");
+//				$("#jqGrid").jqGrid('setGridParam',{ 
+//					datatype:'json', 
+//					url: '../yunying/daishouqujian/list',
+//		            postData: getParams()
+//	            }).trigger("reloadGrid");
+//			}
+//		
+//	}
+//});
 
 function getParams(){
 	var params = {
