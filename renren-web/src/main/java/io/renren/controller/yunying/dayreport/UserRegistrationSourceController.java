303bcfb1d2a57475b9abfbcf8dccf2697eaf79d4
aa5520731 <wanghanzhao@mindai.com>
2017-09-25 15:25:30 +0800

    用户注册来源

---
+++ renren-web/src/main/java/io/renren/controller/yunying/dayreport/UserRegistrationSourceController.java
@@ -0,0 +1,154 @@
+package io.renren.controller.yunying.dayreport;
+
+import static io.renren.utils.ShiroUtils.getUserId;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.aspectj.util.FileUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+
+import io.renren.service.UserBehaviorService;
+import io.renren.system.jdbc.DataSourceFactory;
+import io.renren.system.jdbc.JdbcUtil;
+import io.renren.util.UserBehaviorUtil;
+import io.renren.utils.ExcelUtil;
+import io.renren.utils.PageUtils;
+import io.renren.utils.R;
+
+@Controller
+@RequestMapping(value = "/yunying/source")
+public class UserRegistrationSourceController {
+
+
+	@Autowired
+	private DataSourceFactory dataSourceFactory;
+	@Autowired
+	private UserBehaviorService userBehaviorService;
+
+	private  String reportType="用户注册来源";
+
+	
+	/**
+	 * P2P列表
+	 */
+	@ResponseBody
+	@RequestMapping("/list")
+	public R daylist(Integer page, Integer limit ,String period) {
+		UserBehaviorUtil userBehaviorUtil = new UserBehaviorUtil(userBehaviorService);
+		userBehaviorUtil.insert(getUserId(),new Date(),"查看",reportType," ");
+		
+		String month="";
+		
+		if (StringUtils.isNotEmpty(period)) {
+			period = period.replace("-", "");
+			month = period.substring(0,6);
+		}
+		
+		long l1 = System.currentTimeMillis();
+
+		List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
+
+		try {
+			String path = this.getClass().getResource("/").getPath();
+			String detail_sql;
+			detail_sql = FileUtil.readAsString(new File(path + File.separator + "sql/用户注册来源.txt"));
+			detail_sql = detail_sql.replace("${month}", month);
+			detail_sql = detail_sql.replace("${period}", period);
+			List<Map<String, Object>> list = new JdbcUtil(dataSourceFactory, "oracle26").query(detail_sql);
+			resultList.addAll(list);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int total = resultList.size();
+		PageUtils pageUtil = new PageUtils(resultList, total, limit, page);
+		long l2 = System.currentTimeMillis();
+
+		System.err.println("+++++++用户注册来源数据查询耗时：" + (l2 - l1));
+		return R.ok().put("page", pageUtil);
+	}
+	
+	
+
+	@SuppressWarnings("unchecked")
+	@ResponseBody
+	@RequestMapping("/exportExcel")
+	public void exportMonthListExcel(String params, HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		UserBehaviorUtil userBehaviorUtil = new UserBehaviorUtil(userBehaviorService);
+		userBehaviorUtil.insert(getUserId(),new Date(),"导出",reportType," ");
+		Map<String, Object> map = JSON.parseObject(params, Map.class);
+		String period = map.get("period") + "";
+		R r=daylist(1, 1000000, period);
+		PageUtils pageUtil = (PageUtils) r.get("page");	
+		
+		List<Map<String,Object>> resultList = (List<Map<String, Object>>) pageUtil.getList();
+
+		// 查询列表数据
+		JSONArray va = new JSONArray();
+		for (int i = 0; i < resultList.size(); i++) {
+			va.add(resultList.get(i));
+		}
+		Map<String, String> headMap = null;
+		String title = "用户注册来源";
+		headMap = getDayListExcelFields();
+
+		ExcelUtil.downloadExcelFile(title, headMap, va, response);
+	}
+
+	
+	
+	
+	private Map<String, String> getDayListExcelFields() {
+
+		Map<String, String> headMap = new LinkedHashMap<String, String>();
+
+		headMap.put("HUIZONG", "注册来源");
+		headMap.put("ALL_REG", "注册人数");
+		headMap.put("M_REG", "当月注册");
+		headMap.put("D_REG", "当日注册");
+		headMap.put("ALL_FIRST", "首投人数");
+		headMap.put("M_FIRST", "当月首投人数");
+		headMap.put("D_FIRST", "当日首投人数");
+		
+		headMap.put("M_FIRST_INV", "当月首投金额");
+		headMap.put("D_FIRST_INV", "当日首投金额");
+		headMap.put("WEIZHI4", "当月充值");
+		headMap.put("M_WI", "当月提现");
+		headMap.put("WEIZHI3", "当月净充值");
+		headMap.put("WEIZHI2", "当日充值");
+		headMap.put("D_WI", "当日提现");
+		
+		headMap.put("WEIZHI1", "当日净充值");
+		headMap.put("M_INV", "当月投资");
+		headMap.put("D_INV", "当日投资");
+		headMap.put("AWIAT", "待收本金");
+		
+
+		return headMap;
+
+	}
+
+
+
+
+
+}
---
+++ renren-web/src/main/webapp/js/yunying/user_registration_source.js
@@ -0,0 +1,170 @@
+$(function () {
+	initDetailTableGrid();
+	initExportFunction();
+	initEvent();
+	initTimeCond();
+//	initSelectEvent();
+});
+
+
+//function initSelectEvent(){
+//	//日报，月报切换
+//	$("#list_select").change(function(){
+//		var select = $(this).children('option:selected').val();
+//		if(select == 'vip_detail'){
+//			$("#vip_detail_div").show();
+//			$("#vip_count_div").hide();
+//		}else if(select == 'nianlin'){
+//			$("#vip_detail_div").hide();
+//			$("#vip_count_div").show();
+//		}
+//	});
+//}
+function initTimeCond(){
+    $("#period").datetimepicker({
+        format: 'yyyy-mm-dd',
+        minView:'month',
+        language: 'zh-CN',
+        autoclose:true
+    }).on("click",function(){
+    });
+    $("#period").val(addDate(getCurrDate(), -1));
+}
+
+
+
+function initExportFunction(){
+	$('#btn_exports').click(function(){
+		var params = getParams();
+		
+		var select = $("#list_select").children('option:selected').val();
+			executePost('../yunying/source/exportExcel', {'params' : JSON.stringify(params)});
+		
+	});
+
+}
+function initDetailTableGrid(){
+    $("#jqGrid").jqGrid({
+//        url: '../yunying/dmreportvipuser/list',
+        datatype: "json",
+        colModel: [
+        	{ label: '注册来源', name: 'HUIZONG', frozen : true,index: '$TYPE', width: 150,align:'left', sorttype: 'date'},
+			{ label: '注册人数', name: 'ALL_REG', index: '$NUM', width: 150 ,align:'right'}, 			
+			{ label: '当月注册', name: 'M_REG', index: '$SUM', width: 150 ,align:'right'}, 
+			
+			{ label: '当日注册', name: 'D_REG', index: '$BORROW_USER', width: 150 ,align:'right'}, 			
+			{ label: '首投人数', name: 'ALL_FIRST', index: '$BORROW_CAPITAL', width: 150 ,align:'right'}, 
+			
+			{ label: '当月首投人数', name: 'M_FIRST', index: '$BORROW_USER', width: 150,align:'right'}, 			
+			{ label: '当日首投人数', name: 'D_FIRST', index: '$BORROW_CAPITAL', width: 150 ,align:'right'} ,
+			
+			
+			{ label: '当月首投金额', name: 'M_FIRST_INV', index: '$TYPE', width: 150,align:'left' },
+			{ label: '当日首投金额', name: 'D_FIRST_INV', index: '$NUM', width: 150 ,align:'right'}, 			
+			{ label: '当月充值', name: 'WEIZHI4', index: '$SUM', width: 150 ,align:'right'}, 
+			
+			{ label: '当月提现', name: 'M_WI', index: '$BORROW_USER', width: 150 ,align:'right'}, 			
+			{ label: '当月净充值', name: 'WEIZHI3', index: '$BORROW_CAPITAL', width: 150 ,align:'right'}, 
+			
+			{ label: '当日充值', name: 'WEIZHI2', index: '$BORROW_USER', width: 150,align:'right'}, 			
+			{ label: '当日提现', name: 'D_WI', index: '$BORROW_CAPITAL', width: 150 ,align:'right'} ,
+			
+			
+			{ label: '当日净充值', name: 'WEIZHI1', index: '$BORROW_USER', width: 150 ,align:'right'}, 			
+			{ label: '当月投资', name: 'M_INV', index: '$BORROW_CAPITAL', width: 150 ,align:'right'}, 
+			
+			{ label: '当日投资', name: 'D_INV', index: '$BORROW_USER', width: 150,align:'right'}, 			
+			{ label: '待收本金', name: 'AWIAT', index: '$BORROW_CAPITAL', width: 150 ,align:'right'} 
+		
+	
+        ],
+		viewrecords: true,
+        height: $(window).height()-150,
+        rowNum: 1000,
+        rownumbers: true, 
+        autowidth:true,
+        shrinkToFit: false,
+//        autoScroll: false,
+//        multiselect: false,
+        pager: "#jqGridPager",
+        jsonReader : {
+            root: "page.list",
+            page: "page.currPage",
+            total: "page.totalPage",
+            records: "page.totalCount"
+        },
+        prmNames : {
+            page:"page", 
+            rows:"limit", 
+            order: "order"
+        },
+        gridComplete:function(){
+        	//隐藏grid底部滚动条
+        }
+    });
+    jQuery("#jqGrid").jqGrid('setFrozenColumns');  //冻结列
+
+}
+
+
+
+
+//var vm = new Vue({
+//	el:'#rrapp',
+//	data:{
+//		showList: true,
+//		title: null,
+//		dmReportDdzRemain: {}
+//	},
+//	methods: {
+//		reload: function (event) {
+//			vm.showList = true;
+//			var select = $("#list_select").children('option:selected').val();
+//			if(select == 'vip_detail'){
+//				$("#jqGrid").jqGrid("clearGridData");
+//				$("#jqGrid").jqGrid('setGridParam',{ 
+//					datatype:'json', 
+//					url: '../yunying/daishouqujian/list',
+//		            postData: getParams()
+//	            }).trigger("reloadGrid");
+//			}else if(select == 'nianlin'){
+//				$("#jqGrid_count").jqGrid("clearGridData");
+//				$("#jqGrid_count").jqGrid('setGridParam',{ 
+//					datatype:'json', 
+//					url: '../yunying/daishouqujian/ddylist',
+//		            postData: getParams()
+//	            }).trigger("reloadGrid");
+//			}
+//		}
+//	}
+//});
+
+
+var vm = new Vue({
+	el:'#rrapp',
+	data:{
+		showList: true,
+		title: null,
+		dmReportDdzRemain: {}
+	},
+	methods: {
+		reload: function (event) {
+			vm.showList = true;
+			var select = $("#list_select").children('option:selected').val();
+				$("#jqGrid").jqGrid("clearGridData");
+				$("#jqGrid").jqGrid('setGridParam',{ 
+					datatype:'json', 
+					url: '../yunying/source/list',
+		            postData: getParams()
+	            }).trigger("reloadGrid");
+			}
+		
+	}
+});
+
+function getParams(){
+	var params = {
+			'period': $("#period").val()
+	};
+	return params;
+}
