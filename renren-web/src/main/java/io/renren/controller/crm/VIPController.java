5caa82284764826bde259bb171a4c210f16e8ee1
huguang <huguang@mindai.com>
2017-10-16 18:50:37 +0800

    vip列表

---
+++ renren-web/src/main/java/io/renren/controller/crm/VIPController.java
@@ -0,0 +1,85 @@
+package io.renren.controller.crm;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
+
+import io.renren.system.jdbc.DataSourceFactory;
+import io.renren.system.jdbc.JdbcUtil;
+import io.renren.utils.PageUtils;
+import io.renren.utils.R;
+
+/** 
+ * created by HUGUANG 2017年10月16日 下午4:06:44 
+ */
+
+@RestController
+@RequestMapping("/crm/vip")
+public class VIPController {
+
+	private static final Logger logger = LoggerFactory.getLogger(VIPController.class);
+	
+	@Resource
+	private DataSourceFactory dataSourceFactory;
+	
+	/**
+	 * 查询价值分类
+	 * @author HUGUANG 2017年10月16日下午4:13:51
+	 */
+	@ResponseBody
+	@RequestMapping("/list")
+	@RequiresPermissions("crm:vip:list")
+	public R list(CallrecordQueryParam param, Integer page, Integer limit){
+		logger.info("查询VIP记录,param={},page={},limit={}",JSON.toJSONString(param),page,limit);
+		String[] selectSql = new String[] {
+				"SELECT vu.user_id, vu.dep_user_id, vu.user_name, vu.real_name, vu.gender, vu.phone,",
+				"vub.recover_account_wait,vui.recover_account_wait,vui.account_balance,vui.level,vub.value_type,fa.real_name AS belong_real_name,cr.remark,cr.comm_remark,vui.data_date",
+				"fa.`real_name` as belong_real_name,t.`remark` "
+		};
+		String[] fromSql = new String[]{
+				"FROM vip_user_indicator vui ",
+				"LEFT JOIN vip_user vu ON vu.user_id = vui.user_id ",
+				"LEFT JOIN call_record cr ON cr.user_id = vui.user_id ",
+				"LEFT JOIN vip_user_belongs vub ON vu.user_id = vub.user_id ",
+				"LEFT JOIN financial_advisor fa ON fa.id = vub.belongs_to "
+		};
+		//查询列表的语句
+		StringBuilder querySql = new StringBuilder();
+		querySql.append(StringUtils.join(selectSql)).append(StringUtils.join(fromSql));
+		querySql.append(param.toWhereSql());
+		querySql.append(" ORDER BY cr.id DESC");
+		querySql.append(" LIMIT ").append((page-1)*limit).append(",").append(limit);
+		String sql = querySql.toString();
+		String countSql = "select count(1) as count " + StringUtils.join(fromSql) + param.toWhereSql();
+		logger.info("查询VIP管理列表,SQL={}", sql);
+		logger.info("查询VIP列表,count SQL={}", countSql);
+		JdbcUtil ju = new JdbcUtil(dataSourceFactory, "crmMysql");
+		try {
+			List<Map<String, Object>> list = ju.query(sql);
+			ju = new JdbcUtil(dataSourceFactory, "crmMysql");
+			List<Map<String, Object>> list1 = ju.query(countSql);
+			int count = 0;
+			if(list != null && !list1.isEmpty()){
+				count = Integer.valueOf(list1.get(0).get("count").toString());
+			}
+			PageUtils pageUtil = new PageUtils(list, count, limit, page);
+			return R.ok().put("page", pageUtil);
+		} catch (SQLException e) {
+			logger.error("查询VIP列表失败", e);
+		}
+		return R.error("查询VIP列表失败");
+	}
+}
---
+++ renren-web/src/main/java/io/renren/controller/crm/VIPQueryParam.java
@@ -0,0 +1,165 @@
+package io.renren.controller.crm;
+
+import java.io.Serializable;
+
+/**
+ * 构建VIP列表查询条件
+ * @author HUGUANG 2017年10月16日下午4:13:51
+ */
+public class VIPQueryParam implements Serializable {
+	private String user_id;
+	private String dep_user_id;
+	private String remark;
+	private String comm_remark;
+	private String level;
+	private String belongs_to;
+	private String value_class;
+	private String tags;
+	private String vip_select;
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+
+
+	public String getDep_user_id() {
+		return dep_user_id;
+	}
+
+
+
+	public void setDep_user_id(String dep_user_id) {
+		this.dep_user_id = dep_user_id;
+	}
+
+
+
+	public String getRemark() {
+		return remark;
+	}
+
+
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+
+
+	public String getComm_remark() {
+		return comm_remark;
+	}
+
+
+
+	public void setComm_remark(String comm_remark) {
+		this.comm_remark = comm_remark;
+	}
+
+
+
+	public String getLevel() {
+		return level;
+	}
+
+
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
+
+
+	public String getBelongs_to() {
+		return belongs_to;
+	}
+
+
+
+	public void setBelongs_to(String belongs_to) {
+		this.belongs_to = belongs_to;
+	}
+
+
+
+	public String getValue_class() {
+		return value_class;
+	}
+
+
+
+	public void setValue_class(String value_class) {
+		this.value_class = value_class;
+	}
+
+
+
+	public String getTags() {
+		return tags;
+	}
+
+
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+
+
+	public String getVip_select() {
+		return vip_select;
+	}
+
+
+
+	public void setVip_select(String vip_select) {
+		this.vip_select = vip_select;
+	}
+
+
+
+	String toWhereSql(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("where 1=1 ");
+/*		if(StringUtils.isNotBlank(user_id)){
+			sb.append(" and t.user_id=").append(user_id);
+		}
+		if(StringUtils.isNotBlank(dep_user_id)){
+			sb.append(" and v.dep_user_id=").append(dep_user_id);
+		}
+		if(StringUtils.isNotBlank(belongs_to)){
+			sb.append(" and vb.belongs_to=").append(belongs_to);
+		}
+		if(StringUtils.isNotBlank(status)){
+			sb.append(" and t.status=").append(status);
+		}
+		if(StringUtils.isNotBlank(call_time_type)){
+			int days = 0;
+			if("4".equals(call_time_type)){
+				if(StringUtils.isNotBlank(call_time_s)){
+					sb.append(" and DATE_FORMAT(t.`call_time`,'%Y-%m-%d')>='").append(call_time_s).append("'");
+				}
+				if(StringUtils.isNotBlank(call_time_e)){
+					sb.append(" and DATE_FORMAT(t.`call_time`,'%Y-%m-%d')<='").append(call_time_e).append("'");
+				}
+			}else {
+				if("1".equals(call_time_type)){
+					days = 60;
+				}else if("2".equals(call_time_type)){
+					days = 30;
+				}else if("3".equals(call_time_type)){
+					days = 7;
+				}
+				sb.append(" and to_days(DATE_ADD(t.`call_time`,INTERVAL ").append(days).append(" DAY)) >= to_days(now())");
+			}
+		}*/
+		return sb.toString();
+	}
+}
